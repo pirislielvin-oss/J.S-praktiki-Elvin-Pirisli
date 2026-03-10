c7ed292a39f0f8a6814347d65ef170d3b0437eae
pirislielvin-oss <pirislielvin@gmail.com>
2026-03-10 12:00:29 -0700

    tapsiriqlar

---
+++ index.js
@@ -0,0 +1,79 @@
+/*
+var a =10
+let b =20
+const c =30
+
+console.log(a)
+console.log(b)
+console.log(c)
+*/
+
+/*burda bize en sonuncu yazdigimiz deyeri verecek
+var a =10
+var a=20
+console.log(a)
+*/
+
+/*eror
+let b =20
+let b=30
+console.log(b)
+*/
+
+/*bu formada ise bize let en sonuncu yazdigimiz deyeri verecek
+let b
+b=20
+b=30
+console.log(b)
+*/
+
+/*eror
+const c =30
+const c =40
+console.log(c)
+*/
+
+/*bu formada cont bize yeniden eror verecek. Yeni biz constdan deyismez deyer menimsetmek ucun isledirik
+const b 
+b=20
+b=30
+console.log(b)
+*/
+
+/* bu hisse sehife her yenilendikde ad teleb edir
+let username =prompt("Adinizi daxil edin")
+console.log("salam" +username)
+*/
+
+/* (+) burda number tipinde oldugunu gosterir eger olazsa string tipinde olacaq
+let userage =+prompt("yasinizi daxil edin")
+console.log(typeof userage)
+*/
+
+
+/*
+//Riyazi operatorlar
+let usernumber =10
+let another =0
+console.log(usernumber -5)//cixma
+console.log(usernumber +5)//toplama
+console.log(usernumber *5)//vurma
+console.log(usernumber /5)//bolme
+console.log(usernumber %3)//qaliq alma
+usernumber =parsInt(usernumber) //bu formada edende biz usernumber deyiskeninin tipini numbera cevire bilerik
+console.log(parsInt(usernumber/3))//bu formada edende biz usernumber deyiskeninin tipini numbera cevire bilerik
+*/
+
+
+
+//Mentiq operatorlari
+/*
+&& - ve operatoru
+|| - veya operatoru
+! - deyil operatoru
+
+== - beraberdir operatoru
+=== - hem tip hem de deyer beraberdir operatoru
+!= - beraberdir deyil operatoru
+!== - hem tip hem de deyer beraberdir deyil operatoru
+*/

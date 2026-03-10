feb102d1e13e5b212da9cfa846ed8134ec0a4fdd
pirislielvin-oss <pirislielvin@gmail.com>
2026-03-10 12:16:47 -0700

    elave

---
+++ index2.js
@@ -0,0 +1,75 @@
+/*
+var a =10
+let b =20
+const c =30
+
+console.log(a)
+console.log(b)
+console.log(c)
+*/
+
+/*
+let b =10
+let b=20
+console.log(b)
+*/
+
+/*
+let b
+b=20
+b=30
+console.log(b)
+*/
+
+/*
+const b=30
+const b =40
+console.log(b)
+*/
+
+/*
+const b
+b=20
+b=30
+console.log(b)
+*/
+
+/*
+let username =prompt("Adinizi daxil edin")
+console.log("Salam" +username)
+*/
+
+/*
+let userage =+prompt("Yasinizi daxil edin")
+console.log(typeof userage)
+*/
+
+/*
+let userage = +prompt("Yasinizi daxil edin")
+if(userage >=18){
+    console.log("Siz bu sehifeye daxil ola bilersiniz")
+}else{
+    console.log("Siz bu sehifeye daxil ola bilmersiniz")
+}
+*/
+
+//Riyazi operatorlar
+/*
+let number =10
+let another =0
+console.log(number -5)
+console.log(number +5)
+console.log(number *5)
+console.log(number/5)
+console.log(number %3)
+console.log(number =parseInt(number/3)) 
+*/
+
+//Mentiq operatorlari
+
+
+for(let i=0; i<10; i++){
+    //console.log(i%2==0)
+    console.log(i||i%2==0)
+}
+
